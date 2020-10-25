565e715262c65c1e0f77adb61a65172c4e7a1c13
铠桤 庄 <706747351@qq.com>
2020-10-25 21:20:59 +1100

    Add project files.

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents, HttpPostedFileBase postedFileBase)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("zhuangkaiqi94@gmail.com");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            if (postedFileBase != null && postedFileBase.ContentLength > 0)
+            {
+                string theFileName = Path.GetFileName(postedFileBase.FileName);
+                byte[] fileBytes = new byte[postedFileBase.ContentLength];
+                using (BinaryReader theReader = new BinaryReader(postedFileBase.InputStream))
+                {
+                    fileBytes = theReader.ReadBytes(postedFileBase.ContentLength);
+                }
+                string dataAsString = Convert.ToBase64String(fileBytes);
+                msg.AddAttachment(theFileName, dataAsString);
+            }
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
